d37c8f03e102a9589480d76c491519cf48e2e234
ArmanHZ <arman97@gmail.com>
2025-04-10 01:09:16 -0500

    Error page, favicon and meta image.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import dayilar from "$lib/assets/dayilar.jpg";
+</script>
+
+<div class="main-content">
+    <div style="text-align: center;">
+        <img alt="Dayılar" src={dayilar} />
+        <h1>Are you lost?</h1>
+    </div>
+</div>
